223e0cb573feb3fc6b80984a9b49286eba629cb6
Marco Valeri <marcovaleri@hotmail.it>
2023-02-05 16:12:22 +0000

    Add home template

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,6 +1,14 @@
+// Models
+const Article = require('../models/Article');
+
 exports.home = (req, res, next) => {
-    res.render('./pages/home', {
-        pageTitle: 'Home',
-        pageUrl: req.url
-    })
+    Article.fetchAll()
+        .then(([rows, fields]) => {
+            res.render('./pages/home', {
+                pageTitle: 'Home',
+                pageUrl: req.url,
+                allArticles: rows
+            })
+        })
+        .catch(err => console.log(err));
 }
