d56764796291fdebf85416a73613e3e1cd2330ec
Marco Valeri <marcovaleri@hotmail.it>
2023-02-06 07:28:46 +0000

    Add 404

--- app.js
+++ app.js
@@ -45,6 +45,10 @@ app.use(articleRoutes);
 app.use(pageRoutes);
 app.use(homeRoutes);
 
+app.use((req, res, next) => {
+    res.status(404).send('<h1>404</h1>');
+})
+
 const PORT = process.env.PORT || process.env.SERVER_PORT;
 
 app.listen(PORT);
